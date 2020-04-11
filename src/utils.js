13ee81ec05691762a41054a62d38ed31dde4cc5d
Chuck LeDuc Díaz <chuck@caelumlabs.com>
2020-04-11 17:14:57 +0200

    remove unused helper

--- src/utils.js
+++ src/utils.js
@@ -42,33 +42,6 @@ class Utils {
     }
     return utf8
   }
-
-  static fromUTF8Array (data) { // array of bytes
-    var str = ''
-    var i
-
-    for (i = 0; i < data.length; i++) {
-      var value = data[i]
-
-      if (value < 0x80) {
-        str += String.fromCharCode(value)
-      } else if (value > 0xBF && value < 0xE0) {
-        str += String.fromCharCode((value & 0x1F) << 6 | data[i + 1] & 0x3F)
-        i += 1
-      } else if (value > 0xDF && value < 0xF0) {
-        str += String.fromCharCode((value & 0x0F) << 12 | (data[i + 1] & 0x3F) << 6 | data[i + 2] & 0x3F)
-        i += 2
-      } else {
-        // surrogate pair
-        var charCode = ((value & 0x07) << 18 | (data[i + 1] & 0x3F) << 12 | (data[i + 2] & 0x3F) << 6 | data[i + 3] & 0x3F) - 0x010000
-
-        str += String.fromCharCode(charCode >> 10 | 0xD800, charCode & 0x03FF | 0xDC00)
-        i += 3
-      }
-    }
-
-    return str
-  }
 }
 
 module.exports = Utils
